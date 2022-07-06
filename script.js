4ec0ab03ba8a56a6696df6b870907d69a8398d9c
João Márcio <joaomarciogodoy@gmail.com>
2022-07-06 19:23:24 -0300

    send error

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ newQuote()
 const whatsappBtn = document.getElementById('whats');
 
 function whatsapp() {
-    const whatsappUrl = `https://api.whatsapp.com/share?text=${quoteText.textContent}${authorText.textContent}`;
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${quoteText.textContent}${authorText.textContent}`;
     window.open(whatsappUrl, '_blank');
 }
 
